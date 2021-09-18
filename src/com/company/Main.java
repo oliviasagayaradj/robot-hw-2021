0ba62fb9600f35514fa5f1d6cb8cbe1a518293c0
Jasmine Gomes <jasminegomesusa@gmail.com>
2021-09-18 10:43:23 -0700

    Submitting lLesson 1.1 Primitive Data Types

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+    static int my_age;
+    static String my_name;
+    static int caleb_age;
+    public static void main(String[] args) {
+	// write your code here
+        my_age = 14;
+        my_name = "Jasmine";
+        caleb_age = 16;
+        System.out.println(my_age);
+        System.out.println(my_name);
+        System.out.println(my_age * 12 + " months");
+        System.out.println(caleb_age - my_age);
+    }
+}
